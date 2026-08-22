df541a5995cb49437f438891fbc329dd71ee3c4f
snehadpatel <100825630+snehadpatel@users.noreply.github.com>
2026-08-22 15:39:56 +0530

    refactor: improve itinerary data validation using empty checks and remove unused Recharts dependency

--- includes/footer.php
+++ includes/footer.php
@@ -5,8 +5,6 @@
     <!-- React & ReactDOM -->
     <script src="https://unpkg.com/react@18/umd/react.production.min.js" crossorigin></script>
     <script src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js" crossorigin></script>
-    <!-- Recharts -->
-    <script src="https://unpkg.com/recharts/umd/Recharts.js" crossorigin></script>
     <!-- Global JS utilities -->
     <script src="<?= SITE_URL ?>/assets/js/main.js"></script>
 </body>
